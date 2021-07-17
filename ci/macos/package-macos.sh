25e8b82ceecb027f49d975646d019c1744b52b16
Norihiro Kamae <norihiro@nagater.net>
2021-07-17 17:35:10 +0900

    ci(macos): notarize zip and dmg packages

--- ci/macos/package-macos.sh
+++ ci/macos/package-macos.sh
@@ -13,11 +13,12 @@ if [ "${OSTYPE}" != "Darwin" ]; then
 fi
 
 echo "=> Preparing package build"
-GIT_HASH=$(git rev-parse --short HEAD)
-GIT_TAG=$(/usr/bin/git describe --tags)
+GIT_TAG=$(git describe --tags --long --always)
+GIT_TAG_ONLY=$(git describe --tags --always)
 
-FILENAME_UNSIGNED="$PLUGIN_NAME-${GIT_TAG}-Unsigned.pkg"
-FILENAME="$PLUGIN_NAME-${GIT_TAG}.pkg"
+if test "$GIT_TAG" == "$GIT_TAG_ONLY"; then
+	RELEASE_MODE='false-by-git-tag'
+fi
 
 echo "=> Modifying $PLUGIN_NAME.so"
 install_name_tool \
@@ -30,30 +31,37 @@ install_name_tool \
 	./build/$PLUGIN_NAME.so
 
 # Check if replacement worked
-echo "=> Dependencies for $PLUGIN_NAME"
-otool -L ./build/$PLUGIN_NAME.so
-
-if [[ "$RELEASE_MODE" == "True" ]]; then
-	echo "=> Signing plugin binary: $PLUGIN_NAME.so"
-	codesign --sign "$CODE_SIGNING_IDENTITY" ./build/$PLUGIN_NAME.so
-else
-	echo "=> Skipped plugin codesigning"
-fi
+for dylib in ./build/$PLUGIN_NAME.so lib/*.dylib ; do
+	test -f "$dylib" || continue
+	chmod +r $dylib
+	echo "=> Dependencies for $(basename $dylib)"
+	otool -L $dylib
+	echo
+	if [[ "$RELEASE_MODE" == "True" ]]; then
+		echo "=> Signing plugin binary: $dylib"
+		codesign --sign "$CODE_SIGNING_IDENTITY" $dylib
+	else
+		echo "=> Skipped plugin codesigning since RELEASE_MODE=$RELEASE_MODE"
+	fi
+done
 
 echo "=> ZIP package build"
 ziproot=package-zip/$PLUGIN_NAME
 zipfile=${PLUGIN_NAME}-${GIT_TAG}-macos.zip
+rm -rf ${ziproot:?}/
 mkdir -p $ziproot/bin
 cp ./build/$PLUGIN_NAME.so $ziproot/bin/
+cp LICENSE data/LICENSE-$PLUGIN_NAME
 cp -a data $ziproot/
 mkdir -p ./release
+rmdir lib || mv lib $ziproot/
 (cd package-zip && zip -r ../release/$zipfile $PLUGIN_NAME)
 
 echo "=> DMG package build"
 if pip3 install dmgbuild || pip install dmgbuild; then
 	sed \
 		-e "s;%PLUGIN_NAME%;$PLUGIN_NAME;g" \
-		-e "s;%VERSION%;${GIT_TAG};g" \
+		-e "s;%VERSION%;${GIT_TAG_ONLY};g" \
 		-e "s;%PLUGIN_ROOT%;$ziproot;g" \
 		< ci/macos/package-dmg.json.template > package-dmg.json
 	dmgbuild "$PLUGIN_NAME ${GIT_TAG}" "release/${PLUGIN_NAME}-${GIT_TAG}-macos.dmg" -s ./package-dmg.json
@@ -62,47 +70,48 @@ fi
 # echo "=> Actual package build"
 # packagesbuild ./installer/installer-macOS.generated.pkgproj
 
-# echo "=> Renaming $PLUGIN_NAME.pkg to $FILENAME"
-# mv ./release/$PLUGIN_NAME.pkg ./release/$FILENAME_UNSIGNED
-
 if [[ "$RELEASE_MODE" == "True" ]]; then
-	echo "=> Signing installer: $FILENAME"
-	productsign \
-		--sign "$INSTALLER_SIGNING_IDENTITY" \
-		./release/$FILENAME_UNSIGNED \
-		./release/$FILENAME
-	rm ./release/$FILENAME_UNSIGNED
+	:> requests
 
-	echo "=> Submitting installer $FILENAME for notarization"
-	zip -r ./release/$FILENAME.zip ./release/$FILENAME
-	UPLOAD_RESULT=$(xcrun altool \
-		--notarize-app \
-		--primary-bundle-id "$MACOS_BUNDLEID" \
-		--username "$AC_USERNAME" \
-		--password "$AC_PASSWORD" \
-		--asc-provider "$AC_PROVIDER_SHORTNAME" \
-		--file "./release/$FILENAME.zip")
-	rm ./release/$FILENAME.zip
-
-	REQUEST_UUID=$(echo $UPLOAD_RESULT | awk -F ' = ' '/RequestUUID/ {print $2}')
-	echo "Request UUID: $REQUEST_UUID"
-
-	echo "=> Wait for notarization result"
-	# Pieces of code borrowed from rednoah/notarized-app
-	while sleep 30 && date; do
-		CHECK_RESULT=$(xcrun altool \
-			--notarization-info "$REQUEST_UUID" \
+	for FILENAME in $zipfile ${PLUGIN_NAME}-${GIT_TAG}-macos.dmg; do
+		echo "=> Submitting package $FILENAME for notarization"
+		UPLOAD_RESULT=$(xcrun altool \
+			--notarize-app \
+			--primary-bundle-id "$MACOS_BUNDLEID" \
 			--username "$AC_USERNAME" \
 			--password "$AC_PASSWORD" \
-			--asc-provider "$AC_PROVIDER_SHORTNAME")
-		echo $CHECK_RESULT
+			--asc-provider "$AC_PROVIDER_SHORTNAME" \
+			--file "./release/$FILENAME")
 
-		if ! grep -q "Status: in progress" <<< "$CHECK_RESULT"; then
-			echo "=> Staple ticket to installer: $FILENAME"
-			xcrun stapler staple ./release/$FILENAME
-			break
-		fi
+		REQUEST_UUID=$(echo $UPLOAD_RESULT | awk -F ' = ' '/RequestUUID/ {print $2}')
+		echo "Request UUID: $REQUEST_UUID"
+		echo "$REQUEST_UUID $FILENAME" >> requests
 	done
+
+	t=10
+	while read -u 3 REQUEST_UUID FILENAME; do
+		echo "=> Wait for notarization result of $REQUEST_UUID $FILENAME"
+		# Pieces of code borrowed from rednoah/notarized-app
+		while sleep $t && date; do
+			CHECK_RESULT=$(xcrun altool \
+				--notarization-info "$REQUEST_UUID" \
+				--username "$AC_USERNAME" \
+				--password "$AC_PASSWORD" \
+				--asc-provider "$AC_PROVIDER_SHORTNAME")
+			echo $CHECK_RESULT
+
+			if ! grep -q "Status: in progress" <<< "$CHECK_RESULT"; then
+				echo "$CHECK_RESULT" >> release/${PLUGIN_NAME}-${GIT_TAG}-macos-codesign.log
+				if expr "$FILENAME" : '.*dmg$'; then
+					echo "=> Staple ticket to installer: $FILENAME"
+					xcrun stapler staple ./release/$FILENAME
+				fi
+				t=1
+				break
+			fi
+			t=10
+		done
+	done 3< requests
 else
 	echo "=> Skipped installer codesigning and notarization"
 fi
