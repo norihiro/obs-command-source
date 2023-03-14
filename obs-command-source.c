6c47ec0d4296468e415c600c7165b99de9dfe3d1
Norihiro Kamae <norihiro@nagater.net>
2023-03-14 17:51:29 +0900

    Add plugin-macros.h.in
    The macro will replace `blog` to show the plugin name.
    Also added copyright comment in the source code.

--- obs-command-source.c
+++ obs-command-source.c
@@ -1,3 +1,21 @@
+/*
+ * OBS Command Source Plugin
+ * Copyright (C) 2020-2023 Norihiro Kamae <norihiro@nagater.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <obs-module.h>
 #include <obs-frontend-api.h>
 #include <util/platform.h>
@@ -11,6 +29,7 @@
 #include <signal.h>
 #include <stdlib.h>
 #endif
+#include "plugin-macros.generated.h"
 
 struct command_source
 {
